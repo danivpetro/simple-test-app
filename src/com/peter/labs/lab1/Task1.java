b4a4bf0ba06b64cf92b46c11114c24e1e13d5c02
Peter <danivpetro@gmail.com>
2020-03-21 19:14:17 +0200

    Task 1 lab 1 refactor

--- src/com/peter/labs/lab1/Task1.java
+++ src/com/peter/labs/lab1/Task1.java
@@ -1,7 +1,7 @@
 package com.peter.labs.lab1;
 
 public class Task1 {
-    public static void main (String args  []) {
+    public static void main(String[] args) {
         System.out.println("Привіт! Я Данів Петро це моя перша програма");
     }
 }
