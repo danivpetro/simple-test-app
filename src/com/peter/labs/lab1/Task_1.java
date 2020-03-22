28c81c11b20d8df55bccccee874a418a378a17f3
Peter <danivpetro@gmail.com>
2020-03-22 14:56:55 +0200

    Done Task_2 - lab1

---
+++ src/com/peter/labs/lab1/Task_1.java
@@ -0,0 +1,7 @@
+package com.peter.labs.lab1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("Це ваше перше завдання :)");
+    }
+}
---
+++ src/com/peter/labs/lab1/Task_2.java
@@ -0,0 +1,12 @@
+package com.peter.labs.lab1;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int someveriable;
+        someveriable = 2020;
+        System.out.println("Значення, яке Ви присвоїли змінній рівне:"
+                + someveriable);
+        someveriable = someveriable - 1990;
+        System.out.println("Вам зараз " + someveriable + " років");
+    }
+}
