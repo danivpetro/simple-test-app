2801872f4e2da6336e11d5fe3fd8cee3f61b652e
Peter <danivpetro@gmail.com>
2020-03-23 21:21:07 +0200

    Done Task_5 - lab1

---
+++ src/com/peter/labs/lab1/Task_5.java
@@ -0,0 +1,21 @@
+package com.peter.labs.lab1;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        int mount_day[];
+        mount_day = new int[12];
+        mount_day[0] = 31;
+        mount_day[1] = 28;
+        mount_day[2] = 31;
+        mount_day[3] = 30;
+        mount_day[4] = 31;
+        mount_day[5] = 30;
+        mount_day[6] = 31;
+        mount_day[7] = 30;
+        mount_day[8] = 31;
+        mount_day[9] = 30;
+        mount_day[10] = 31;
+        mount_day[11] = 30;
+        System.out.println("У місяці лютоу" + mount_day[1] + "");
+    }
+}
