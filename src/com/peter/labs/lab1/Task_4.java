2b69433c3ca5303b9e75d6e24d9d90c3153a7d46
Peter <danivpetro@gmail.com>
2020-03-23 20:39:09 +0200

    Done Task_4 - lab1

---
+++ src/com/peter/labs/lab1/Task_4.java
@@ -0,0 +1,11 @@
+package com.peter.labs.lab1;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        int zminna;
+        System.out.println("Покрокове значення змінної рівне");
+        for (zminna = -5; zminna <= 5; zminna = ++) {
+            System.out.println("Значення рівне +zminna");
+        }
+    }
+}
